9887b5baef32d0f202f99bfd05ad55d0f7f1c435
Md. Rabbi Molla <118678096+mdrabbimolla@users.noreply.github.com>
2024-01-20 16:00:28 +0600

    Update script.js
    Update again

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Web Designer", "Real Estate Executive"],
+        strings: ["Web Developer", "Web Designer", "Real Estate Services"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Web Designer", "Real Estate Executive"],
+        strings: ["Web Developer", "Web Designer", "Real Estate Services"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
